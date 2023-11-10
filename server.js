24c5d8fe180ab8dd0b0f7ebeaae3fa883e4861be
piyawat pintusornsri <piyawat@softever.co.th>
2023-11-10 18:06:52 +0700

    change host

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // MySQL Connection Pool
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "root",
   database: "mysql_express",
