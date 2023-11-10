1fc1fbd21399d7aa8505a8b126411259168cc3be
piyawat pintusornsri <piyawat@softever.co.th>
2023-11-10 14:46:06 +0700

    Allowed CORS policy

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mysql = require("mysql");
-
+const cors = require("cors");
 const app = express();
 
 //Middleware
+app.use(cors());
 app.use(express.json());
 
 // MySQL Connection
